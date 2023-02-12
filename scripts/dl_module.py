b76fc19abc62c129c2473c09ceed36baf4374b4e
poyo23 <suo.s.games@gmail.com>
2023-02-12 09:09:11 +0900

    bug fix

--- scripts/dl_module.py
+++ scripts/dl_module.py
@@ -18,7 +18,7 @@ def donwload_images(dl_grid_images):
         symbol_outputs=os.path.join(main_dir,"temp_outputs_for_dl")
         os.makedirs(symbol_outputs,exist_ok=True)
         # delete symlink if exist
-        _path_list = [os.path.join(symbol_outputs,f) for f in os.listdir(outputs_dir)]
+        _path_list = [os.path.join(symbol_outputs,f) for f in os.listdir(symbol_outputs)]
         link_path_list = [p for p in _path_list if not os.path.islink(p)]
         for lp in link_path_list:
             os.unlink(lp)
